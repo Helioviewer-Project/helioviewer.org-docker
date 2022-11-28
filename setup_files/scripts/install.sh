665103eef14d4b1c329429645e74d8f5ba84380f
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2022-11-28 11:04:49 -0500

    Update how container sets itself up on clean install (#8)
    * Update references to latest
    
    * Align readme and run.sh to use the same container name
    
    * Add platform to run.sh
    
    * Update scripts so they don't use `su www-data`

--- setup_files/scripts/install.sh
+++ setup_files/scripts/install.sh
@@ -6,7 +6,7 @@ rm 2021.zip
 
 # Set up Kakadu inside the api folder.
 cd $API_DIR/install/kakadu
-su www-data -s /bin/bash -c 'tar zxvpf Kakadu_v6_4_1-00781N_Linux-64-bit-Compiled.tar.gz'
+tar zxvpf Kakadu_v6_4_1-00781N_Linux-64-bit-Compiled.tar.gz
 mv lib/* /usr/local/lib/
 mv bin/* /usr/local/bin/
 /sbin/ldconfig
