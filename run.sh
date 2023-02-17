3554182fac5b03673bbb114064ca892b19b2e085
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-02-17 13:59:08 -0500

    Update run.sh to use docker tag

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ JP2_PATH=`pwd`/sample-data
 API_PATH=`pwd`/api
 HELIOVIEWER_PATH=`pwd`/helioviewer.org
 #CONTAINER=helioviewer-dev:test
-CONTAINER=testing
+CONTAINER=dgarciabriseno/helioviewer.org
 
 docker run --platform linux/x86_64 -p 127.0.0.1:8080:80 -p 127.0.0.1:8081:81 -v "$API_PATH:/home/helioviewer/api.helioviewer.org" -v "$HELIOVIEWER_PATH:/home/helioviewer/helioviewer.org" -d -t $CONTAINER
