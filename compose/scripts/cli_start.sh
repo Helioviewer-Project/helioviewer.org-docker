4bcc32a309aaee3f777fe3fce70801899f36c141
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2023-12-14 11:17:41 -0500

    Download test data via admin container

--- compose/scripts/cli_start.sh
+++ compose/scripts/cli_start.sh
@@ -5,7 +5,12 @@ cp /home/admin/2021_06_01__00_01_29_132__SDO_AIA_AIA_304.jp2 /tmp/jp2
 
 SETTINGS_DIR=/home/admin/api/install/settings
 sed "s|dbhost = localhost|dbhost = database|" ${SETTINGS_DIR}/settings.example.cfg > ${SETTINGS_DIR}/settings.cfg
+sed -i "s|/mnt/data/hvpull|/tmp|" ${SETTINGS_DIR}/settings.cfg
 sed -i "s|/mnt/data/jp2|/tmp/jp2|" ${SETTINGS_DIR}/settings.cfg
 sed -i "s|server = localhost|server=|" ${SETTINGS_DIR}/settings.cfg
+
+# Download some test data
+cd /home/admin/api/install
+python downloader.py -d hv_soho -s "2023-12-01 00:00:00" -e "2023-12-01 01:00:00"
 
 tail -f /dev/null
