a3339b70bd4f79b8160c13f9f502d867315a5544
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-02-09 14:29:53 -0500

    Change underscore to - for k8

--- compose.prod/scripts/downloader.sh
+++ compose.prod/scripts/downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd ~/api/install
-cp /run/secrets/api_settings settings/settings.cfg
+cp /run/secrets/api-settings settings/settings.cfg
 python downloader.py $@
--- compose.prod/scripts/web.prod.startup.sh
+++ compose.prod/scripts/web.prod.startup.sh
@@ -1,7 +1,7 @@
 set -e
-cp /run/secrets/api_config /var/www/api.helioviewer.org/settings/Config.ini
-cp /run/secrets/api_private /var/www/api.helioviewer.org/settings/Private.php
-cp /run/secrets/config_js /var/www/html/resources/js/Utility/Config.js
+cp /run/secrets/api-config /var/www/api.helioviewer.org/settings/Config.ini
+cp /run/secrets/api-private /var/www/api.helioviewer.org/settings/Private.php
+cp /run/secrets/config-js /var/www/html/resources/js/Utility/Config.js
 cd /var/www/html/resources/build
 ant
 source /etc/apache2/envvars
