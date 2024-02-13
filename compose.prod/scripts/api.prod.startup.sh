d70728a532c972c7f585387d4adf5312b1faa7c3
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-02-13 15:01:34 -0500

    Remove `_` in secret names to support kompose (#30)
    * Change underscore to - for k8
    
    * Restore github workflow

--- compose.prod/scripts/api.prod.startup.sh
+++ compose.prod/scripts/api.prod.startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 set -e
-cp /run/secrets/api_config /var/www/api.helioviewer.org/settings/Config.ini
-cp /run/secrets/api_private /var/www/api.helioviewer.org/settings/Private.php
+cp /run/secrets/api-config /var/www/api.helioviewer.org/settings/Config.ini
+cp /run/secrets/api-private /var/www/api.helioviewer.org/settings/Private.php
 
 export PATH=$PATH:/tmp/miniforge3/bin
 READY_FILE=/tmp/container_ready
