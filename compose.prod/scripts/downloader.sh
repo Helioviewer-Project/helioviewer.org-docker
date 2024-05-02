a129e48270db3463baebf030f76f780d3c2ea7f5
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-05-02 11:10:11 -0400

    Remove compose.prod support
    There are no plans to use this at this time and no desire to support it.
    If you wish to continue development on this, check out the tag `prod-compose`

--- compose.prod/scripts/downloader.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd ~/api/install
-cp /run/secrets/api-settings settings/settings.cfg
-python downloader.py $@
--- compose.prod/scripts/web.prod.startup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-cp /run/secrets/api-config /var/www/api.helioviewer.org/settings/Config.ini
-cp /run/secrets/api-private /var/www/api.helioviewer.org/settings/Private.php
-cp /run/secrets/config-js /var/www/html/resources/js/Utility/Config.js
-cd /var/www/html/resources/build
-ant
-source /etc/apache2/envvars
-/usr/sbin/apache2 -DFOREGROUND
