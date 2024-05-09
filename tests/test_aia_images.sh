7324f3c3638b7dc06e288244337fd57d795ad291
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-05-09 15:56:12 -0400

    Fix test

--- tests/test_aia_images.sh
+++ tests/test_aia_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # This test verifies that there is an AIA 304 image and AIA 171 image installed in the container
 # The Helioviewer API and "output=minimal" page each expect an AIA 171 to be present.
@@ -7,7 +8,6 @@
 #   2. Verify the AIA 304 image is present
 #   3. Verify the AIA 171 image is present
 
-exit_code=0
 jp2ls=$(docker exec helioviewer-api-1 ls /tmp/jp2)
 echo "ls /tmp/jp2:"
 # Print out the files for debug purposes in case this test fails.
@@ -17,17 +17,10 @@ echo $jp2ls
 aia_images=$(echo $jp2ls | grep AIA)
 
 # 2. Verify the AIA 304 image is present
-img=$(echo $aia_images | grep 304)
-if [ -z $img ]; then
-    echo "Missing AIA 304 image"
-    exit_code=1
-fi
+echo "Checking for AIA 304 image"
+echo $aia_images | grep 304
 
 # 3. Verify the AIA 171 image is present
-aia_171=$(echo $aia_images | grep 171)
-if [ -z $aia_171 ]; then
-    echo "Missing AIA 171 image"
-    exit_code=1
-fi
+echo "Checking for AIA 171 image"
+echo $aia_images | grep 171
 
-exit $exit_code
