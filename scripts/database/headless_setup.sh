784bc09fd0dc6ac1fdb1c2e759f3a66456c70162
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-11-26 09:23:37 -0500

    Update headless_setup to use env vars for db init

--- scripts/database/headless_setup.sh
+++ scripts/database/headless_setup.sh
@@ -2,13 +2,16 @@
 
 set timeout 5
 set root_password $::env(MARIADB_ROOT_PASSWORD)
+set db_name $::env(HELIOVIEWER_DB_NAME)
+set db_user $::env(HELIOVIEWER_DB_USER)
+set db_pass $::env(HELIOVIEWER_DB_PASS)
 spawn python3 install.py
 
 expect "Location" { send "/tmp/jp2\r" }
 expect "Choice" { send "1\r" }
-expect "Database" { send "\r" }
-expect "Username" { send "\r" }
-expect "Password" { send "\r" }
+expect "Database" { send "$db_name\r" }
+expect "Username" { send "$db_user\r" }
+expect "Password" { send "$db_pass\r" }
 expect "Database" { send "database\r" }
 expect "Choice" { send "1\r" }
 expect "Username" { send "root\r" }
--- scripts/database/initialize_database.sh
+++ scripts/database/initialize_database.sh
@@ -1,5 +1,4 @@
 set -e
-mariadb -h database -u root -p"${MARIADB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${SUPERSET_USER}'@'%' IDENTIFIED BY '${SUPERSET_PASS}';"
 cd /scripts
 /setup_scripts/headless_setup.sh
 
